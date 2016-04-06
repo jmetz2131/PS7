3eb0bd8900dde8401255129f32adaaa4a9fa236f
jmetz2131 <jacob.metz@wustl.edu>
2016-04-06 05:50:42 -0500

    First Commit
    Here I am just trying to figure out how the sg.int function actually
    works. I know g has to be defined outside of the function itself but I
    am still confused about how the upper and lower bounds work. Currently
    figuring that out.

--- PS7.R
+++ PS7.R
@@ -1,5 +1,6 @@
 sg.int<-function(g,...,lower,upper)
 
+
 { require("SparseGrid")
 
  lower<-floor(lower)
@@ -29,3 +30,22 @@ sg.int<-function(g,...,lower,upper)
   val.sp <- gx.sp %*%weights
   val.sp
 }
+
+##Here I am installing the SparseGrid package to make sure I can work with this library.
+install.packages("SparseGrid")
+library(SparseGrid)
+
+#I just figured out that the entirety of the code here relates to the sg.int function itself.
+#I also know that I will have to define g outside of the sg.int function to make sg.int actually
+#work
+
+##Defining g. I made g a simple linear function of 4x+5 and checked that it works by making x=2, which
+##appropriately outputted 13.
+g <-function(x){
+  return(4*x + 5)
+}
+g(2)
+
+##Attempting the integration itself with my newly defined g
+sg.int(g, lower=0, upper=4)
+
