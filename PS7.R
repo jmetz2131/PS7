7ef408034c79f48b7cf7f86c36830a47e4278f4d
jmetz2131 <jacob.metz@wustl.edu>
2016-04-14 11:53:47 -0500

    Adding libraries to prepare for dealing with function

--- PS7.R
+++ PS7.R
@@ -1,9 +1,30 @@
+install.packages("SparseGrid")
+library(SparseGrid)
+install.packages("testthat")
+library(testthat)
+library(microbenchmark)
+install.packages("parallel")
+library(parallel)
+install.packages("cubature")
+library(cubature)
+##I'm using these libraries to prepare for actually changing the 
+##function to include things parallel processing and to measure gains in speed
+
+
+##Defining g. I made g a simple linear function of 4x+5 and checked that it works by making x=2, which
+##appropriately outputted 13.
+g <-function(x){
+  return(4*x + 5)
+}
+g(2)
+
 sg.int<-function(g,...,lower,upper)
-
-
+  
 { require("SparseGrid")
 
- lower<-floor(lower)
+ lower<-floor(lower) ##vector of the lower bounds and it will needs to accept any number
+ #based on the number of dimensionality. You should ensure that length of lower=length of upper
+ 
 
  upper<-ceiling(upper)
 
@@ -11,6 +32,8 @@ sg.int<-function(g,...,lower,upper)
 
  gridss<-as.matrix(expand.grid(seq(lower[1],upper[1]-1,by=1),seq(lower[2],upper[2]-1,by=1)))
 
+##From what I can see, I will change the dimensions of the function here by changing the dimension argument to
+##equal something other than 2.
  sp.grid <- createIntegrationGrid( 'KPU', dimension=2, k=5 )
 
  nodes<-gridss[1,]+sp.grid$nodes
@@ -35,17 +58,14 @@ sg.int<-function(g,...,lower,upper)
 install.packages("SparseGrid")
 library(SparseGrid)
 
+##I am now checking the help files for SparseGrid
+?SparseGrid
+
 #I just figured out that the entirety of the code here relates to the sg.int function itself.
 #I also know that I will have to define g outside of the sg.int function to make sg.int actually
 #work
 
-##Defining g. I made g a simple linear function of 4x+5 and checked that it works by making x=2, which
-##appropriately outputted 13.
-g <-function(x){
-  return(4*x + 5)
-}
-g(2)
-
 ##Attempting the integration itself with my newly defined g
 sg.int(g, lower=0, upper=4)
+
 
